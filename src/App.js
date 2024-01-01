3f92d47d873d8e40fed45566e2ba83b2c3597fe2
dvjnpojascodes <deviejanepojas@gmail.com>
2024-01-01 12:46:40 +0800

    fixed routes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Header/>} />
-        <Route path='/Ecochic-Apparel-Website/' element={<Header/>} />
         <Route path='/collection' element={<Collection/>}/>
         {/* <Route path='/coords' element={<ShopCategory banner={coord_banner} category="coord"/>} /> */}
         <Route path='/coat' element={<Coats/>}/>
